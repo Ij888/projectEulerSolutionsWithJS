becdd24435a0138e2a21c4ed3eb0db9a367865a1
Ij888 <ij.ofili@gmail.com>
2015-01-19 15:52:04 +0000

    solution 3

--- solution1.js
+++ solution1.js
@@ -8,7 +8,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 var sum = 0;
 for(k=0; k<1000; k++){
     if(((k%3)===0)||((k%5)===0)){
-    sum += k;
+    sum += k; //what going on here
         console.log(sum);
     }
 }
---
+++ solution3.js
@@ -0,0 +1,13 @@
+//solution to problem set 3 on project euler
+
+/* The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+//var primeHolder =  0;
+
+for (k=0; k<600851475143; k++){
+    if((600851475143%k)===0)
+        console.log(k);
+    }    
